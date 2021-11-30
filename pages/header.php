fe93982679a7c9ad31f9b1b816c393207d5bbb97
dxchml <dchml@outlook.fr>
2021-11-30 12:18:47 +0100

    [ADD] Header, à personnaliser !

--- pages/header.php
+++ pages/header.php
@@ -4,19 +4,47 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link src="css/style.css" rel="stylesheet">
+    <link src="C:\Users\DCHML\OneDrive\Bureau\Travail\IPSSI\3x1\Restoo\css" rel="stylesheet">
 </head>
 <body>
-
     <header>
-        <h1>Connexion et inscription</h1>
-        <nav>
-            <ul>
-                <a href="connexion.php">Connexion</a>
-                <a href="inscription.php">Inscription</a>
-                <a></a>
-
-            </ul>
-        </nav>
+        <div class="container">
+            <img class="logo" href="" src="../asset/")>
+            <a href=""> Accueil </a>
+            <a href=""> Présentation </a>
+            <a href=""> Contact </a>
+        </div>
     </header>
-    
+</body>
+
+<style>
+
+.container {
+    width: 100%;
+    height: 70px;
+    display: flex;
+    position: fixed;
+    font-size: 20px;
+    margin-top: -8px;
+    margin-left: -8px;
+    align-items: center;
+    justify-content: space-between;
+    background: linear-gradient(90deg, black, 90%, white);
+}
+
+.container a {
+    color: white;
+    padding: 10px 100px;
+    text-decoration: none;
+}
+
+.container a:hover {
+    text-decoration: underline;
+    text-shadow: 1px 3px 5px white;
+}
+
+.logo {
+
+}
+
+</style>
