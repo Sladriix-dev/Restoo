5aeee3d952b7b75a3ce5a39f6107c093a029095b
Charlotte Kindt <37578645+Chachalotte@users.noreply.github.com>
2021-12-03 11:56:27 +0100

    PKFKPZPDKPZPKDZKPZD

--- pages/profil/onglets/NotesDonnées.php
+++ pages/profil/onglets/NotesDonnées.php
@@ -1,29 +1,51 @@
+
 <?php 
+
 require_once "../../vendor/autoload.php";
-//include_once "../../App/Entite/User.php";
-//include_once "../../App/Controller/UserProfile.php";
-use App\Controller\UserProfile;
+use App\Controller\UserProfileController;
+
+//Reset le message de validation des modifs d'options
+$_SESSION['MessageOptions'] = "";
 
 ?>
 
 <div class="NotesListe">
-    <h2>Notes de plats :</h2>
+    
+    <center><h2>Notes de Restaurants :</h2></center>
 
 
     <?php
     // Récupération des notes via la classe
     //Notes de restau 
-    $UserProfile = new UserProfile();
+    $UserProfile = new UserProfileController();
     $ListeNotes = $UserProfile->getLoggedUserNotes(1);
-    //while($ligne=$UserProfile->fetch())
+    $IsEmpty = false;
+
+    ?>
+    <table align=center class=ProfilTable>
+        <tbody>
+    <?php
 
     foreach ($ListeNotes as $ligne)
     {
-        echo "<b>".$ligne->restau_nom."</b> <i>(".$ligne->note_score.")</i> ";
+        $IsEmpty = true;
+        echo "<tr><td>Vous avez donné la note de <b>".$ligne->note_score."/10</b> au restaurant <i>".$ligne->restau_nom."</i> </td></tr>";
         //echo $ligne['restaurant.restau_nom'] . " : (" . $ligne['note.note_score'] . ") ";
         // var_dump($ligne->context);
         echo "<br/>";
     }
+
+    ?>
+        </tbody>
+    </table>
+
+        <?php
+
+    if ($IsEmpty == false)
+    {
+        echo "<br/>(Vous n'avez pas noté le restaurant)";
+    }
+
     echo "<br/>";
 
 
@@ -33,17 +55,35 @@ use App\Controller\UserProfile;
 <br/>
 
 <div class="NotesListe">
-    <h2>Notes de Recettes :</h2>
+    <center><h2>Notes de Recettes :</h2></center>
 
     <?php
 
     $ListeNotes = $UserProfile->getLoggedUserNotes(2);
+    $IsEmpty = false;
+
+    ?>
+<table align=center class=ProfilTable>
+    <tbody>
+    <?php
 
     foreach ($ListeNotes as $ligne)
     {
-        echo "<b>".$ligne->rec_nom."</b> <i>(".$ligne->note_score.")</i><br /> ";   
+        $IsEmpty = true;
+        echo "	<tr><td>Vous avez donné la note de <b>".$ligne->note_score."/10</b> à notre <i>".$ligne->rec_nom."</i></td></tr><br /> ";   
     }
-    echo "<br />";
+  
+
+    ?>
+        </tbody>
+    </table>
+
+        <?php
+
+    if ($IsEmpty == false)
+    {
+        echo "<br/>(Vous n'avez pas noté nos recettes)";
+    }
 
     ?>
 </div>
